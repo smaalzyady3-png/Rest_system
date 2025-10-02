2b3366b9d753cdc1deee938949608f2678ccaed7
smaalzyady3-png <smaalzyady3@gmail.com>
2025-10-02 13:58:58 +0300

    اضافة المصاريف
    ا

---
+++ add_expense.php
@@ -0,0 +1,286 @@
+<?php
+include("session.php");
+$update = false;
+$del = false;
+$expenseamount = "";
+$expensedate = date("Y-m-d");
+$expensecategory = "Entertainment";
+if (isset($_POST['add'])) {
+    $expenseamount = $_POST['expenseamount'];
+    $expensedate = $_POST['expensedate'];
+    $expensecategory = $_POST['expensecategory'];
+
+    $expenses = "INSERT INTO expenses (user_id, expense,expensedate,expensecategory) VALUES ('$userid', '$expenseamount','$expensedate','$expensecategory')";
+    $result = mysqli_query($con, $expenses) or die("Something Went Wrong!");
+    header('location: add_expense.php');
+}
+
+if (isset($_POST['update'])) {
+    $id = $_GET['edit'];
+    $expenseamount = $_POST['expenseamount'];
+    $expensedate = $_POST['expensedate'];
+    $expensecategory = $_POST['expensecategory'];
+
+    $sql = "UPDATE expenses SET expense='$expenseamount', expensedate='$expensedate', expensecategory='$expensecategory' WHERE user_id='$userid' AND expense_id='$id'";
+    if (mysqli_query($con, $sql)) {
+        echo "Records were updated successfully.";
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+    }
+    header('location: manage_expense.php');
+}
+
+if (isset($_POST['update'])) {
+    $id = $_GET['edit'];
+    $expenseamount = $_POST['expenseamount'];
+    $expensedate = $_POST['expensedate'];
+    $expensecategory = $_POST['expensecategory'];
+
+    $sql = "UPDATE expenses SET expense='$expenseamount', expensedate='$expensedate', expensecategory='$expensecategory' WHERE user_id='$userid' AND expense_id='$id'";
+    if (mysqli_query($con, $sql)) {
+        echo "تم تحديث السجلات بنجاح.";
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+    }
+    header('location: manage_expense.php');
+}
+
+if (isset($_POST['delete'])) {
+    $id = $_GET['delete'];
+    $expenseamount = $_POST['expenseamount'];
+    $expensedate = $_POST['expensedate'];
+    $expensecategory = $_POST['expensecategory'];
+
+    $sql = "DELETE FROM expenses WHERE user_id='$userid' AND expense_id='$id'";
+    if (mysqli_query($con, $sql)) {
+        echo "تم تحديث السجلات بنجاح.";
+    } else {
+        echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+    }
+    header('location: manage_expense.php');
+}
+
+if (isset($_GET['edit'])) {
+    $id = $_GET['edit'];
+    $update = true;
+    $record = mysqli_query($con, "SELECT * FROM expenses WHERE user_id='$userid' AND expense_id=$id");
+    if (mysqli_num_rows($record) == 1) {
+        $n = mysqli_fetch_array($record);
+        $expenseamount = $n['expense'];
+        $expensedate = $n['expensedate'];
+        $expensecategory = $n['expensecategory'];
+    } else {
+        echo ("WARNING: AUTHORIZATION ERROR: Trying to Access Unauthorized data");
+    }
+}
+
+if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $del = true;
+    $record = mysqli_query($con, "SELECT * FROM expenses WHERE user_id='$userid' AND expense_id=$id");
+
+    if (mysqli_num_rows($record) == 1) {
+        $n = mysqli_fetch_array($record);
+        $expenseamount = $n['expense'];
+        $expensedate = $n['expensedate'];
+        $expensecategory = $n['expensecategory'];
+    } else {
+        echo ("WARNING: AUTHORIZATION ERROR: Trying to Access Unauthorized data");
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>اضافة المصاريف</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Feather JS for Icons -->
+    <script src="js/feather.min.js"></script>
+
+</head>
+
+<body>
+
+    <div class="d-flex" id="wrapper">
+
+        <!-- Sidebar -->
+        <div class="border-right" id="sidebar-wrapper">
+            <div class="user">
+                <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="120">
+                <h5><?php echo $username ?></h5>
+                <p><?php echo $useremail ?></p>
+            </div>
+            <div class="sidebar-heading">ادارة</div>
+            <div class="list-group list-group-flush">
+                <a href="index.php" class="list-group-item list-group-item-action"><span data-feather="home"></span> لوحة التحكم</a>
+                <a href="add_expense.php" class="list-group-item list-group-item-action sidebar-active"><span data-feather="plus-square"></span> اضافة المصاريف</a>
+                <a href="manage_expense.php" class="list-group-item list-group-item-action"><span data-feather="dollar-sign"></span> ادارة جميع المصاريف</a>
+            </div>
+            <div class="sidebar-heading">اعدادات </div>
+            <div class="list-group list-group-flush">
+                <a href="profile.php" class="list-group-item list-group-item-action "><span data-feather="user"></span> ملفي الشخصي</a>
+                <a href="logout.php" class="list-group-item list-group-item-action "><span data-feather="power"></span> تسجيل الخروج</a>
+            </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+
+            <nav class="navbar navbar-expand-lg navbar-light  border-bottom">
+
+
+                <button class="toggler" type="button" id="menu-toggle" aria-expanded="false">
+                    <span data-feather="menu"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img class="img img-fluid rounded-circle" src="<?php echo $userprofile ?>" width="25">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="profile.phcol-mdp">ملفي الشخصي</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="logout.php">تسجيل الخروج</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="container">
+                <h3 class="mt-4 text-center">اضافة مصاريف جديد</h3>
+                <hr>
+                <div class="row ">
+
+                    <div class="col-md-3"></div>
+
+                    <div class="col-md" style="margin:0 auto;">
+                        <form action="" method="POST">
+                            <div class="form-group row">
+                                <label for="expenseamount" class="col-sm-6 col-form-label"><b>المبلغ الذي تم صرفه($)</b></label>
+                                <div class="col-md-6">
+                                    <input type="number" class="form-control col-sm-12" value="<?php echo $expenseamount; ?>" id="expenseamount" name="expenseamount" required>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="expensedate" class="col-sm-6 col-form-label"><b>تاريخ صرف المبلغ</b></label>
+                                <div class="col-md-6">
+                                    <input type="date" class="form-control col-sm-12" value="<?php echo $expensedate; ?>" name="expensedate" id="expensedate" required>
+                                </div>
+                            </div>
+                            <fieldset class="form-group">
+                                <div class="row">
+                                    <legend class="col-form-label col-sm-6 pt-0"><b>فئة المصاريف</b></legend>
+                                    <div class="col-md">
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory4" value="مصاريف طبية" <?php echo ($expensecategory == 'Medicine') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory4">
+                                           مصاريف طبية
+                                            </label>
+                                        </div>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory3" value="طعام" <?php echo ($expensecategory == 'Food') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory3">
+                                                طعام
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory2" value="الفواتير وإعادة الشحن" <?php echo ($expensecategory == 'Bills & Recharges') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory2">
+                                            الفواتير وإعادة الشحن
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory1" value="مصروفات" <?php echo ($expensecategory == 'Entertainment') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory1">
+                                               مصروفات
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory7" value="ملابس" <?php echo ($expensecategory == 'Clothings') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory7">
+                                                ملابس
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory6" value="تأجير" <?php echo ($expensecategory == 'Rent') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory6">
+                                            تأجير
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory8" value=" ادوات تنظيف" <?php echo ($expensecategory == 'Household Items') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory8">
+                                                ادوات تنظيف
+                                            </label>
+                                        </div>
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="expensecategory" id="expensecategory5" value="Others" <?php echo ($expensecategory == 'Others') ? 'checked' : '' ?>>
+                                            <label class="form-check-label" for="expensecategory5">
+                                                اشياء اخرى
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                            <div class="form-group row">
+                                <div class="col-md-12 text-right">
+                                    <?php if ($update == true) : ?>
+                                        <button class="btn btn-lg btn-block btn-warning" style="border-radius: 0%;" type="submit" name="update">تحديث</button>
+                                    <?php elseif ($del == true) : ?>
+                                        <button class="btn btn-lg btn-block btn-danger" style="border-radius: 0%;" type="submit" name="delete">حذف</button>
+                                    <?php else : ?>
+                                        <button type="submit" name="add" class="btn btn-lg btn-block btn-success" style="border-radius: 0%;">اضافة مصاريف</button>
+                                    <?php endif ?>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="col-md-3"></div>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/Chart.min.js"></script>
+    <!-- Menu Toggle Script -->
+    <script>
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+    </script>
+    <script>
+        feather.replace();
+    </script>
+    <script>
+
+    </script>
+</body>
+</html>
